cb911be7bbdd2ddfc4fd2752ea785f51ae8636c9
keithagroves <keithalgroves@gmail.com>
2025-12-15 19:27:24 -0800

    chore: release v2.0.19

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -32,7 +32,7 @@ import {
 } from "./commands";
 import { error, formatError } from "./utils";
 
-export const version = "2.0.18";
+export const version = "2.0.19";
 
 // Export types for external use
 export type { GlobalOptions, CommandContext } from "./types";
